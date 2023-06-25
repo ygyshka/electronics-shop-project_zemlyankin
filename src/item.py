a6508a67d8595ec4f528528300bb596df3f7aa1a
WHAT <ygyshka0@gmail.com>
2023-06-25 17:08:56 +0700

    change status object + new func

--- src/item.py
+++ src/item.py
@@ -1,10 +1,14 @@
+import csv
+import os
+
+
 class Item:
     """
     Класс для представления товара в магазине.
     """
     pay_rate = 1.0
     all = []
-
+    Path = '..\src\items.csv'
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
         Создание экземпляра класса item.
@@ -13,11 +17,11 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         self.total_price = self.price * self.quantity
-        pass
+        self.all.append(self)
 
     def calculate_total_price(self) -> float:
         """
@@ -33,3 +37,29 @@ class Item:
         """
         self.price = self.price * Item.pay_rate
 
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, data_str: str):
+
+        name = data_str
+        if len(name) > 10:
+            self.__name = name[:10]
+        else:
+            self.__name = name
+
+    @classmethod
+    def instantiate_from_csv(cls, filename=Path):
+        """
+        Метод-класса инициализирующий экземпляры класса `Item` данными из файла _src/items.csv
+        """
+        with open(filename, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                cls(row['name'], float(row['price']), cls.string_to_number(row['quantity']))
+
+    @staticmethod
+    def string_to_number(number: str):
+        return int(float(number))
