9dae7053118799ac0ecc683768b0fb8ecd4f8336
WHAT <ygyshka0@gmail.com>
2023-06-27 21:28:22 +0700

    add __repr__ and __str__

--- src/item.py
+++ src/item.py
@@ -9,6 +9,7 @@ class Item:
     pay_rate = 1.0
     all = []
     Path = '..\src\items.csv'
+
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
         Создание экземпляра класса item.
@@ -22,6 +23,20 @@ class Item:
         self.quantity = quantity
         self.total_price = self.price * self.quantity
         self.all.append(self)
+
+    def __repr__(self):
+        """
+        Магический метод __repr__ возвращающий информацию экземпляра класса для разработчика
+        """
+
+        return f"{self.__class__.__name__}('{self.__name}', {self.price}, {self.quantity})"
+
+    def __str__(self):
+        """
+        Магический метод __str__ возвращающий информацию экземпляра класса для пользователя
+        """
+
+        return f"'{self.name}'"
 
     def calculate_total_price(self) -> float:
         """
