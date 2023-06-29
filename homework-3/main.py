7d71a5a228ad3565453d5d4f06f44b4d4a50ea37
WHAT <ygyshka0@gmail.com>
2023-06-29 19:05:07 +0700

    edit main

--- homework-3/main.py
+++ homework-3/main.py
@@ -4,6 +4,4 @@ if __name__ == '__main__':
     item1 = Item("Смартфон", 10000, 20)
     assert repr(item1) == "Item('Смартфон', 10000, 20)"
     # Вывел принт чтобы посмотреть наглядно, но не понимаю почему assert выдает ошибку
-    print(repr(item1))
-    print(item1)
     assert str(item1) == 'Смартфон'
