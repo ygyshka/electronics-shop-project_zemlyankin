b5f5c2242b35c0b43704d9badd48b1352e2738d1
WHAT <ygyshka0@gmail.com>
2023-06-29 19:03:27 +0700

    edit main

--- homework-2/main.py
+++ homework-2/main.py
@@ -1,4 +1,5 @@
 from src.item import Item
+import os
 
 if __name__ == '__main__':
     item = Item('Телефон', 10000, 5)
@@ -11,10 +12,8 @@ if __name__ == '__main__':
     item.name = 'СуперСмартфон'
     # Exception: Длина наименования товара превышает 10 символов.
 
-    Item.all = []
     Item.instantiate_from_csv()  # создание объектов из данных файла
     assert len(Item.all) == 5  # в файле 5 записей с данными по товарам
-
     item1 = Item.all[0]
     assert item1.name == 'Смартфон'
 
