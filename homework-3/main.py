afe742ef5428ac7c71b8115a382dc19863bb70a7
WHAT <ygyshka0@gmail.com>
2023-07-09 18:03:40 +0700

    delete notes

--- homework-3/main.py
+++ homework-3/main.py
@@ -3,5 +3,4 @@ from src.item import Item
 if __name__ == '__main__':
     item1 = Item("Смартфон", 10000, 20)
     assert repr(item1) == "Item('Смартфон', 10000, 20)"
-    # Вывел принт чтобы посмотреть наглядно, но не понимаю почему assert выдает ошибку
     assert str(item1) == 'Смартфон'
