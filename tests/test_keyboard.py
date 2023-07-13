3aef6cd7517daf3204af8974d8ebf03c03791743
WHAT <ygyshka0@gmail.com>
2023-07-13 20:59:47 +0700

    edit tests

--- tests/test_keyboard.py
+++ tests/test_keyboard.py
@@ -9,3 +9,10 @@ def test_1():
 
 def test_language(test_1):
     assert test_1.language == 'EN'
+
+
+def test_change_lang(test_1):
+    test_1.change_lang()
+    assert test_1.language == "RU"
+    test_1.change_lang()
+    assert test_1.language == "EN"
